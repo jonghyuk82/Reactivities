5733f536ec3355e9c1e47c5ecfb11a7fd6e7a045
jonghyuk lee <jonghyuklee@Johns-MacBook-Pro.local>
2020-08-28 16:27:39 -0400

    Added MediatR and created Query handler

--- API/Program.cs
+++ API/Program.cs
@@ -28,7 +28,7 @@ namespace API
                     Seed.SeedData(context);
                 }
                 catch(Exception ex)
-                {
+                { 
                     var logger = services.GetRequiredService<ILogger<Program>>();
                     logger.LogError(ex, "An error occured during migration");
                 }
